4b88574c1ddc07fc6f4010e6e09bac8efadacce1
Jackal <jackal@localhost.localdomain>
2020-10-25 12:08:09 -0500

    improved info command

--- commands/info.js
+++ commands/info.js
@@ -2,6 +2,13 @@ module.exports = {
 	name: 'info',
 	description: 'Basic Info about ByteBot.',
 	execute(message,args) {
-		message.channel.send('I am ByteBot.');
+		const Discord = require('discord.js');
+		const embed = new Discord.MessageEmbed()
+		.setColor('#8215d6')
+		.setTitle('ByteBot Info')
+		.setURL('https://github.com/TheJKL/ByteBot')
+		.setFooter('https://github.com/TheJKL/ByteBot', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png');
+
+		message.channel.send(embed);
 	},
 };
