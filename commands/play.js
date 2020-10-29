e9649bf25d2591c5b13667dbdb7b6ea765935cb3
Jackal <jackal@localhost.localdomain>
2020-10-28 22:09:20 -0500

    Play command basic implimentation

--- commands/play.js
+++ commands/play.js
@@ -2,8 +2,10 @@ module.exports = {
 	name: 'play',
 	description: 'description of command',
 	async execute(message,args) {
+		const opus = require('@discordjs/opus');
 		if (message.member.voice.channel){
 			const connection = await message.member.voice.channel.join();
+			connection.play('./sounds/Bundy.wav');
 		}
 	},
 };
